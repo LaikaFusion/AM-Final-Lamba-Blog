e0dbf699ac3aacc1be26931bf799b949b989f42a
LaikaFusion <newrad4@gmail.com>
2019-02-14 12:49:28 -0500

    added mobile menu

--- src/components/Mainmenu.jsx
+++ src/components/Mainmenu.jsx
@@ -1,15 +1,68 @@
 import React, { Component } from 'react'
 import '../styles/MenuBar.css'
 
-const Mainmenu = props => {
-  return (
-    <div className="menubarContainer">
-      <a href="https://andrewmclaughl.in" className="menuButton">Home</a>
-      <a href="https://andrewmclaughl.in/about/" className="menuButton">About</a>
-      <a href="https://andrewmclaughl.in/demos/" className="menuButton">Demos</a>
-      <a className="menuButton selected">Blog</a>
-    </div>
-  )
+class MenuBar extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      open: true,
+    }
+
+    this.toggleOpen = () => {
+      this.setState(prevState => ({
+        open: !prevState.open,
+      }))
+    }
+    this.updateWindowDimensions = () => {
+      if (window.innerWidth < 700) {
+        this.setState({ open: false })
+      } else {
+        this.setState({ open: true })
+      }
+    }
+  }
+  componentDidMount() {
+    this.updateWindowDimensions()
+    window.addEventListener('resize', this.updateWindowDimensions)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions)
+  }
+  render() {
+    return (
+      <div className="menubarContainer">
+        {this.state.open ? (
+          <div className="toplist">
+            <a href="https://andrewmclaughl.in" className="menuButton">
+              Home
+            </a>
+            <a href="https://andrewmclaughl.in/about/" className="menuButton">
+              About
+            </a>
+            <a href="https://andrewmclaughl.in/demos/" className="menuButton">
+              Demos
+            </a>
+            <a className="menuButton selected">Blog</a>{' '}
+          </div>
+        ) : null}
+        <div className="menuArrowContainer">
+          <div onClick={this.toggleOpen} className="menuButton openarrow">
+            Menu
+            <span
+              className={
+                this.state.open
+                  ? 'menuArrow rotated openarrow'
+                  : ' counterrotated menuArrow openarrow'
+              }
+            >
+              ▼
+            </span>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default Mainmenu
+export default MenuBar
